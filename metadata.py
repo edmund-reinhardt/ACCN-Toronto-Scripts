78180b5e677283007b854a7797f736270cc17698
Johanna Reinhardt <Gods.Annie@gmail.com>
2019-06-01 15:32:51 -0400

    Refactored code and added loop through directories
    Signed-off-by: Johanna Reinhardt <Gods.Annie@gmail.com>

--- metadata.py
+++ metadata.py
@@ -3,12 +3,8 @@ from glob import glob
 from os.path import join
 from os.path import basename
 
-for path in glob(join("acc_web", "2013", "*.mp3")):
-    print(path)
 
-    filename = basename(path)
-    print(filename)
-
+def get_date(filename):
     date = f"20{filename[1:3]}-{filename[3:5]}-{filename[5:7]}"
 
     if filename[8:11] == "000":
@@ -16,20 +12,39 @@ for path in glob(join("acc_web", "2013", "*.mp3")):
     elif filename[8:11] == "001":
         date += " PM"
 
-    print(date)
+    return date
 
-    metadata = eyed3.load(path)
 
-    scripture = metadata.tag.album
-    speaker = metadata.tag.artist
-
-    print(scripture)
-    print(speaker)
-
+def make_name(date, scripture, speaker):
     filename = date
 
     for item in (scripture, speaker):
         if item is not None:
             filename += " - " + item
 
-    print(filename)
+    return filename
+
+
+for directory in glob(join("acc_web", "*")):
+    print(directory)
+    for path in glob(join(directory, "*.mp3")):
+        print(path)
+
+        filename = basename(path)
+        print(filename)
+
+        metadata = eyed3.load(path).tag
+
+        if metadata is None:
+            scripture = None
+            speaker = None
+        else:
+            scripture = metadata.album
+            speaker = metadata.artist
+
+        print(scripture)
+        print(speaker)
+
+        filename = make_name(get_date(filename), scripture, speaker)
+
+        print(filename)
