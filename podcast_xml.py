fefc01c80e8766bc57a4c1f0f0fec405023e213a
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2020-03-04 19:56:22 -0500

    Function Add item element to RSS XML

--- podcast_xml.py
+++ podcast_xml.py
@@ -1,12 +1,12 @@
 import xml.etree.ElementTree as ET
 from os.path import basename, getsize
+
 from metadata import get_metadata
 
 
-def create_item_for_mp3(path):
+def add_item(path: str, parent: ET.Element):
     scripture, speaker, date = get_metadata(path)
-    scripture, speaker, date = get_metadata(path)
-    item = ET.Element('item')
+    item = ET.SubElement(parent, 'item')
 
     title = ET.SubElement(item, 'title')
     title.text = f"{date}: {speaker} - {scripture}"
@@ -16,16 +16,20 @@ def create_item_for_mp3(path):
     enclosure.set('type', 'audio/x-mp3')
     summary = ET.SubElement(item, 'itunes:summary')
     summary.text = f"Sunday service by {speaker} on {scripture}"
+
     return item
 
+
 if __name__ == '__main__':
-    # tree = ET.parse('accntoronto_rss.xml')
-    #
-    # print('Hi')
+    tree = ET.parse('accntoronto_rss.xml')
+    # root = tree.getroot()
+
+    # print(root)
     # print(tree.findall("./channel/item"))
-    # channel = tree.find("./rss/channel")
-    # print(channel)
     # for item in tree.getroot().iter('item'): print(item.attrib)
-    path = 'C:\\Users\\Nlisu\\OneDrive\\Desktop\\Current Courses\\Side Project with Church\\ACCN-Toronto-Scripts\\-160103_000.mp3'
-    item = create_item_for_mp3(path)
+    #    path = 'C:\\Users\\Nlisu\\OneDrive\\Desktop\\Current Courses\\Side Project with Church\\ACCN-Toronto-Scripts\\-160103_000.mp3'
+
+    path = 'C:\\Users\\EDMUNDReinhardt\\accweb\\2020\\-200223_001.mp3'
+    item = add_item(path, tree.getroot()[0])
     ET.dump(item)
+    ET.dump(tree)
