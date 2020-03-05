5a26797a65d97d2e2297a0368e2d6facbe349cea
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2020-03-04 21:18:03 -0500

    When synchronizing, also update the RSS XML and upload it if it changed

--- podcast_xml.py
+++ podcast_xml.py
@@ -6,11 +6,13 @@ from xml.dom import minidom
 
 from metadata import get_metadata
 
+XML_FILE = 'accntoronto_rss.xml'
 
-def add_item(path: str, parent: ET.Element):
+
+def add_item(path: str, tree: ET.ElementTree):
+    item = ET.SubElement(tree.getroot().find("channel"), 'item')
+
     scripture, speaker, date = get_metadata(path)
-    item = ET.SubElement(parent, 'item')
-
     title = ET.SubElement(item, 'title')
     title.text = f"{date}: {speaker} - {scripture}"
     enclosure = ET.SubElement(item, 'enclosure')
@@ -32,17 +34,25 @@ def pretty_print(root: ET.Element) -> str:
         )).toprettyxml()
 
 
+def parse_rss_xml():
+    ET.register_namespace("itunes", "http://www.itunes.com/dtds/podcast-1.0.dtd")
+    tree = ET.parse(XML_FILE)
+    return tree
+
+
+def write_rss_xml(tree: ET.ElementTree):
+    with open(XML_FILE, "w") as f:
+        f.write(pretty_print(tree.getroot()))
+
+
 # Generate all of the RSS XML items from the mp3 files in the directory path specified
 # through file dialog
 if __name__ == '__main__':
-    ET.register_namespace("itunes", "http://www.itunes.com/dtds/podcast-1.0.dtd")
-    xml_file = 'accntoronto_rss.xml'
-    tree = ET.parse(xml_file)
+    tree = parse_rss_xml()
 
     directory = filedialog.askdirectory()
     for path in glob(join(directory, "*.mp3")):
         print(path)
-        add_item(path, tree.getroot()[0])
+        add_item(path, tree)
 
-    with open(xml_file, "w") as f:
-        f.write(pretty_print(tree.getroot()))
+    write_rss_xml(tree)
--- sync_sermons.py
+++ sync_sermons.py
@@ -3,6 +3,7 @@ from os import listdir
 from os.path import join
 
 from credentials import SERVER, USERNAME, PASSWORD
+from podcast_xml import parse_rss_xml, add_item, write_rss_xml, XML_FILE
 
 
 def sync_sermons(server_name, user_name, password, remote_path, local_path):
@@ -16,9 +17,17 @@ def sync_sermons(server_name, user_name, password, remote_path, local_path):
         files_to_transfer = list(local_files - remote_files)
         print(f"Found {len(files_to_transfer)} file(s) to transfer")
 
+        tree = parse_rss_xml()
+
         for f in files_to_transfer:
             print(f"Uploading {f}")
             with open(join(local_path, f), "rb") as file_contents:
+                ftp.storbinary('STOR ' + f, file_contents, callback=lambda _: print(".", end=""))
+            add_item(local_path, tree)
+
+        if len(files_to_transfer) != 0:
+            write_rss_xml(tree)
+            with open(join(XML_FILE, f), "rb") as file_contents:
                 ftp.storbinary('STOR ' + f, file_contents, callback=lambda _: print(".", end=""))
 
     print(f"Done uploading {len(files_to_transfer)}")
