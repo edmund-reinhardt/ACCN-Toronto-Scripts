7ba57f4615e5104394c5f937d014e740f8855224
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2020-03-12 23:15:35 -0400

    add pubData and morning/afternoon to description in rss xml

--- podcast_xml.py
+++ podcast_xml.py
@@ -1,4 +1,5 @@
 import xml.etree.ElementTree as ET
+from datetime import datetime
 from glob import glob
 from os.path import basename, getsize, join
 from tkinter import filedialog
@@ -16,15 +17,39 @@ def add_item(path: str, tree: ET.ElementTree):
 
     scripture, speaker, date = get_metadata(path)
     title = ET.SubElement(item, 'title')
-    title.text = f"{date}: {speaker} - {scripture}"
+    title.text = f"{speaker} - {scripture}"
     enclosure = ET.SubElement(item, 'enclosure')
     enclosure.set('url', f"http://accn-toronto.org/media/mp3/sermons/2020/{basename(path)}")
     enclosure.set('length', str(getsize(path)))
     enclosure.set('type', 'audio/x-mp3')
     summary = ET.SubElement(item, 'itunes:summary')
-    summary.text = f"Sunday service by {speaker} on {scripture}"
+    time_of_day = get_time_of_day(date)
+    summary.text = f"Sunday {time_of_day} service by {speaker} on {scripture}"
+
+    pub_date = ET.SubElement(item, 'pubData')
+    pub_date.text= get_full_date(date)
 
     return item
+
+
+def get_time_of_day(date: str)->str:
+    time_day = date.split(" ")[1]
+    if time_day == "AM":
+        return "morning"
+    else:
+        return "afternoon"
+
+
+def get_full_date(date: str):
+    am_or_pm = date.split(" ")
+    date1 = am_or_pm[0].split("-")
+    print(date1[0])
+    full_date_time = datetime(int(date1[0]), int(date1[1]), int(date1[2]))
+    if am_or_pm[1] == "AM":
+        time = "10:30:00"
+    else:
+        time = "14:30:00"
+    return full_date_time.strftime(f"%A, %d %b %Y {time} EST")
 
 
 def pretty_print(root: ET.Element) -> str:
