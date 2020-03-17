ba5a777f0629584b682435455216c27130b0be48
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2020-03-17 09:50:17 -0400

    add no extra lines in accntoronto_rss.xml

--- tests/test_podcast_xml.py
+++ tests/test_podcast_xml.py
@@ -12,12 +12,35 @@ def test_get_full_date():
     assert "Sun, 19 Jan 2020 10:30:00 -0500" == get_full_date('2020-01-19 AM')
 
 
+def test_strip_empty_lines():
+    assert "a"+"\n"+"b" == strip_empty_lines("\na\nb\n")
+
+
 def test_add_item():
     tree = parse_rss_xml()
     add_item("-200126_001.mp3", tree)
     result = pretty_print(tree)
-    print(result)
-
-
-def test_strip_empty_lines():
-    assert "a"+"\n"+"b" == strip_empty_lines("\na\nb\n")
+    assert """<?xml version="1.0" ?>
+<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
+	<channel>
+		<title>ACC Toronto Services</title>
+		<itunes:author>Toronto Apostolic Christian Church (Nazarean)</itunes:author>
+		<itunes:owner>
+			<itunes:email>accntoronto@gmail.com</itunes:email>
+		</itunes:owner>
+		<itunes:image href="http://accn-toronto.org/media/mp3/sermons/podcastImage.png"/>
+		<itunes:summary>
+            Sermons and other audio from Apostolic Christian Church in Toronto, ON
+        </itunes:summary>
+		<language>en</language>
+		<itunes:explicit>no</itunes:explicit>
+		<link>http://accn-toronto.org</link>
+		<itunes:category text="Religion &amp; Spirituality"/>
+		<item>
+			<title>Phillip Denzinger - John 16</title>
+			<enclosure url="http://accn-toronto.org/media/mp3/sermons/2020/-200126_001.mp3" length="24780946" type="audio/x-mp3"/>
+			<itunes:summary>Sunday afternoon service by Phillip Denzinger on John 16</itunes:summary>
+			<pubDate>Sun, 26 Jan 2020 14:30:00 -0500</pubDate>
+		</item>
+	</channel>
+</rss>""" == result
