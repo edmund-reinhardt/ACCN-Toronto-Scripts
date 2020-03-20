494c6b6e362f1ab60125051db6047fc307530910
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2020-03-19 20:52:08 -0400

    added individual item test to add_item in test_podcast_xml.py

--- tests/test_podcast_xml.py
+++ tests/test_podcast_xml.py
@@ -1,7 +1,8 @@
 """Test all the functionality in podcast_xml"""
 from os.path import join, dirname
 
-from accweb.podcast_xml import add_item, parse_rss_xml, pretty_print, strip_empty_lines, get_verbose_am_pm, get_full_date
+from accweb.podcast_xml import add_item, parse_rss_xml, pretty_print, strip_empty_lines, get_verbose_am_pm, \
+    get_full_date
 
 
 def test_get_verbose_am_pm():
@@ -15,12 +16,20 @@ def test_get_full_date():
 
 
 def test_strip_empty_lines():
-    assert "a"+"\n"+"b" == strip_empty_lines("\na\nb\n")
+    assert "a" + "\n" + "b" == strip_empty_lines("\na\n\nb\n")
 
 
 def test_add_item():
+    import xml.etree.ElementTree as ET
     tree = parse_rss_xml()
-    add_item(join(dirname(__file__), "-200126_001.mp3"), tree)
+    item = add_item(join(dirname(__file__), "-200126_001.mp3"), tree)
+    assert '<item>' \
+           '<title>Phillip Denzinger - John 16</title>' \
+           '<enclosure url="http://accn-toronto.org/media/mp3/sermons/2020/-200126_001.mp3" length="24780946" type="audio/x-mp3" />' \
+           '<itunes:summary>Sunday afternoon service by Phillip Denzinger on John 16</itunes:summary>' \
+           '<pubDate>Sun, 26 Jan 2020 14:30:00 -0500</pubDate>' \
+           '</item>' == \
+           ET.tostring(item, encoding='unicode', method="xml")
     result = pretty_print(tree)
     assert """<?xml version="1.0" ?>
 <rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" version="2.0">
