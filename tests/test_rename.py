1e74fd7bb81b644e216c754176c2027bd083f358
Edmund Reinhardt <edmund.reinhardt@ca.ibm.com>
2020-03-19 21:45:34 -0400

    failing test case

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -7,3 +7,6 @@ def test_make_name():
     assert """2020-02-14 PM - Jovan Lisulov.mp3""" == make_name("2020-02-14 PM",None,"Jovan Lisulov")
     assert """2020-02-14 PM.mp3""" == make_name("2020-02-14 PM",None,None)
     assert """2020-02-14 PM - Matt 1 vs1.mp3""" == make_name("2020-02-14 PM","Matt 1:1",None)
+
+def test_fail():
+    assert make_name("2020-02-14 PM","Matt 1:1",None) == """2020-02-14 PM - Matt 1.mp3"""
